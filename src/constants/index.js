922365919f4d42177c2e810363ce3401e9521c0f
ThainaSantoss <thainados_santos@hotmail.com>
2025-04-30 16:01:19 -0300

    first commit portfolio pessoal

---
+++ src/constants/index.js
@@ -0,0 +1,65 @@
+// import project1 from "../assets/projects/project-1.jpg";
+// import project2 from "../assets/projects/project-2.jpg";
+// import project3 from "../assets/projects/project-3.jpg";
+// import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `Estudante de Análise e Desenvolvimento de Sistemas na UNIFG, com foco em front-end e experiência em projetos que conectam pessoas à cultura por meio da tecnologia. Estou em busca de uma oportunidade para transformar ideias em código e crescer junto com times que fazem a diferença.`;
+
+export const ABOUT_TEXT = `Sou estudante de Análise e Desenvolvimento de Sistemas na UNIFG, com foco em criar soluções web inovadoras e eficientes. Tenho experiência anterior como Jovem Aprendiz, onde desenvolvi habilidades em organização, comunicação e resolução de problemas — competências que hoje aplico no universo da tecnologia.
+Meu principal interesse é atuar no desenvolvimento front-end, utilizando ferramentas como React, TypeScript, TailwindCSS e Git. Participei da criação da plataforma Desbrave, um projeto acadêmico que conecta jovens ao patrimônio cultural de Recife através da tecnologia.
+Além disso, venho me qualificando por meio de cursos em tecnologias modernas e fundamentos de cloud computing com AWS. Estou em busca de uma oportunidade de estágio onde possa crescer profissionalmente, contribuir em equipe e evoluir como desenvolvedor.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Mar/2023 – Jul/2024",
+    role: "Jovem Aprendiz",
+    company: "Braspress Transportes Urgentes",
+    description: `Apoio em avidades administravas, como organização de documentos e atendimento a clientes.`,
+    technologies: [ "Resolução de Problemas", "Comunicação eficaz", "Gestão de tempo"],
+  },
+  
+];
+
+export const PROJECTS = [
+  {
+    title: "Plataforma Desbrave",
+    // image: project1,
+    description:
+      "Desenvolvimento de uma plataforma web voltada à cultura e educação em Recife,conectando jovens ao patrimônio cultural da cidade e promovendo cidadania digital.",
+    technologies: ["React", "JavaScript", "Bootstrap", "Node.js"],
+  },
+  {
+    title: "Galeria de Fotos",
+    // image: project2,
+    description:
+      "que exibe uma galeria interativa de imagens com funcionalidades de slider principal, navegação por miniaturas e um modal de exibição em tela cheia.",
+    technologies: ["Next.js", "TypeScipt", "TailwindCSS"],
+  },
+  {
+    title: "Mind Quiz",
+    // image: project3,
+    description:
+      "quiz de raciocínio lógico que ajuda os usuários a testarem suas habilidades, oferecendo feedback imediato sobre a porcentagem de acertos.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Projeto Delivery Pizza",
+    // image: project2,
+    description:
+      "Projeto simples e interativo que permite aos usuários escolherem seus sabores de pizza preferidos e realizar pedidos online.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+  },
+  {
+    title: "Weather Forecast",
+    // image: project4,
+    description:
+      "permite aos usuários obter previsões meteorológicas em tempo real para qualquer cidade. O projeto apresenta uma interface amigável e intuitiva, que muda de cor automaticamente de acordo com o horário do dia.",
+    technologies: ["HTML", "CSS", "OpenWeather", "JavaScript"],
+  },
+];
+
+export const CONTACT = {
+  address: "Recife, Pernambuco",
+  phoneNo: "+55 81 97315-6023",
+  email: "thainados_santos@hotmail.com",
+};
